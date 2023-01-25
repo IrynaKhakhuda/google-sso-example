7390c582a69f9b88ce4aa8f6d18df535da45828b
Iryna_Khakhuda <iryna_khakhuda@epam.com>
2023-01-25 22:53:49 +0200

    Google sso works

---
+++ src/main/java/com/example/springsso/SpringSsoApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.format.DateTimeFormatter;
+
+@SpringBootApplication
+public class SpringSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoApplication.class, args);
+    }
+
+}
